f4e3ac706ae316efa034db21994cbfa219327107
everardo sanchez <everardo.ipn@gmail.com>
2016-02-07 15:18:48 -0600

    feat: redirect workshops

--- app/components/app-menu.js
+++ app/components/app-menu.js
@@ -12,6 +12,10 @@ export default Ember.Component.extend({
     goConferences() {
       this.set('isMenuVisible', false);
       window.location.href = 'conferences/-K9j2vWu2cXoMfhXS7Io';
+    },
+    goWorkshops() {
+      this.set('isMenuVisible', false);
+      window.location.href = 'workshops/-K9pZdS9XtbrFUz9nXUv';
     }
   }
 });
