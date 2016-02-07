8f99dcba59a3b88b402248c9c569f70dce5180ee
everardo sanchez <everardo.ipn@gmail.com>
2016-02-07 02:26:14 -0600

    fix: add image to model

--- app/models/activity.js
+++ app/models/activity.js
@@ -5,6 +5,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   dateTime: DS.attr('date'),
   language: DS.attr('string'),
+  image: DS.attr('string'),
   person: DS.attr(),
   isConference: Ember.computed('type',function() {
     return this.get('type') === 'C'
