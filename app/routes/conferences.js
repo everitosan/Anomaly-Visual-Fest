1027ec09f6efd44e4bdfc9f6447dac869e6428ae
everardo sanchez <everardo.ipn@gmail.com>
2016-02-08 00:35:31 -0600

    fix: sintaxis missing semicolon

--- app/routes/conferences.js
+++ app/routes/conferences.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.filter("activity", {filter: { type: 'C' });
+    return this.store.query("activity", {filter: { type: 'C' }});
   }
   // ,
   // afterModel(model, params) {
