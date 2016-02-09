c842ae79cfab56d8ebb808dd90dd9633e679ba05
everardo sanchez <everardo.ipn@gmail.com>
2016-02-09 14:02:38 -0600

    fix: share url

--- app/components/social-share.js
+++ app/components/social-share.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   url: Ember.computed(function() {
-    //return "guarded-mesa-44249.herokuapp.com/conferences/-K9k4MwOaOABwEkrXPpx";
-    return window.location.host + window.location.pathname;
+    return window.location.href;
   })
 });
