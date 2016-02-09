ca742cf4dcac1753bbcadcc9195ca676964de7ce
everardo sanchez <everardo.ipn@gmail.com>
2016-02-09 13:52:15 -0600

    feat: share buttons

--- app/components/social-share.js
+++ app/components/social-share.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   url: Ember.computed(function() {
-    return "guarded-mesa-44249.herokuapp.com/conferences/-K9k4MwOaOABwEkrXPpx";
-    //return window.location.host + window.location.pathname;
+    //return "guarded-mesa-44249.herokuapp.com/conferences/-K9k4MwOaOABwEkrXPpx";
+    return window.location.host + window.location.pathname;
   })
 });
