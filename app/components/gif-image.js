8815d3eb7e7bfb4020411b5bb5a0af03934b3b10
everardo sanchez <everardo.ipn@gmail.com>
2016-02-12 15:13:06 -0600

    refact: gif-image component url

--- app/components/gif-image.js
+++ app/components/gif-image.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   computedStyle: Ember.computed('imageUrl', function() {
     let imageUrl = this.get('imageUrl');
+    imageUrl = "https://guarded-mesa-44249.herokuapp.com/assets/img/" + imageUrl;
+
+    this.set('imageUrl', imageUrl);
     return Ember.String.htmlSafe("background: url("+imageUrl+")");
   })
 });
