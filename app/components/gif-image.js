6a7e78cf28e144aee60834331d4178d214587e59
everardo sanchez <everardo.ipn@gmail.com>
2016-02-16 01:48:25 -0600

    refact: gif-image component

--- app/components/gif-image.js
+++ app/components/gif-image.js
@@ -1,11 +1,33 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
 
 export default Ember.Component.extend({
   computedStyle: Ember.computed('imageUrl', function() {
     let imageUrl = this.get('imageUrl');
-    imageUrl = "https://guarded-mesa-44249.herokuapp.com/assets/img/" + imageUrl;
+    let _self = this;
 
-    this.set('imageUrl', imageUrl);
-    return Ember.String.htmlSafe("background: url("+imageUrl+")");
+    imageUrl = "assets/img/" + imageUrl;
+    if(ENV.environment === "production") {
+      imageUrl = this.assets.resolve(imageUrl);
+    }
+
+    let imagePromise = new Ember.RSVP.Promise(function(resolve, reject) {
+      let im = new Image();
+      im.src=imageUrl;
+      im.onload=function(){
+        resolve(this);
+      };
+    }).then(function(image){
+      let fix_scale = (_self.get('type') === "profile" )? 1.2 : 1;
+      let proportion = parseInt(image.height/(image.width*fix_scale)*100);
+
+
+      _self.set('computedStyle', Ember.String.htmlSafe("background: url("+imageUrl+"); padding-bottom:"+ proportion +"%;") );
+    }, function(reason) {
+      console.log(reason);
+    });
+
+
+    return Ember.String.htmlSafe("background: #000");
   })
 });
