40f42ff6bd052ef150872d27e904730ddd4a85da
everardo sanchez <everardo.ipn@gmail.com>
2016-02-17 15:26:28 -0600

    refact: calendar to make hover animation of background

--- app/components/footer-button.js
+++ app/components/footer-button.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['footer-button'],
   isCalendar: Ember.computed('type', function() {
     return this.get('type') === "Calendar";
-  })
+  }),
+  mouseEnter() {
+    let $element = $(this.get('element'));
+    $element.addClass('hover');
+  },
+  mouseLeave() {
+    let $element = $(this.get('element'));
+    $element.removeClass('hover');
+  }
 });
