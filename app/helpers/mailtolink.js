bdd6b49569679609f3268f43187d0b10edc8b12a
everardo sanchez <everardo.ipn@gmail.com>
2016-02-21 18:43:08 -0600

    refact: updates for link class

--- app/helpers/mailtolink.js
+++ app/helpers/mailtolink.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
 export function mailtolink(params) {
-  let mailTo = '<a href="mailto:' + params[1] + '">';
-   mailTo += params[0] + '</a>';
-   return new Ember.Handlebars.SafeString(mailTo);
+  let mailTo = '<a data-text="'+params[0]+'" href="mailto:' + params[1]+'"';
+  if (params[2]){
+    mailTo += "class='"+params[2]+"'";
+  }
+  mailTo +='>';
+  mailTo += params[0] + '</a>';
+
+  return new Ember.Handlebars.SafeString(mailTo);
 }
 
 export default Ember.Helper.helper(mailtolink);
