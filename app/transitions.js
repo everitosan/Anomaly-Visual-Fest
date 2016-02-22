3ba54c49e18948a097f9b85b9106a27c8de337d2
everardo sanchez <everardo.ipn@gmail.com>
2016-02-21 19:28:07 -0600

    feat: animates all routes

--- app/transitions.js
+++ app/transitions.js
@@ -1,7 +1,7 @@
 export default function(){
   let duration = 0;
   this.transition(
-    this.fromRoute('index'),
+    this.fromRoute(['index', 'conferences', 'workshops', 'calendar', 'tickets', 'location']),
     this.toRoute(['conferences', 'workshops', 'calendar', 'tickets', 'location']),
     this.use('toLeft', {duration}),
     this.reverse('toRight', {duration})
